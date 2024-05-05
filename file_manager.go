e538854b3eb7644498a1f3653d935f61a6b59a7f
flameweaver0x <163330032+flameweaver0x@users.noreply.github.com>
2024-05-05 13:03:22 +0200

    new update

--- file_manager.go
+++ file_manager.go
@@ -3,12 +3,14 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"crypto/sha256"
 	"fmt"
 	"io"
 	"log"
 	"math"
 	"os"
 	"strconv"
+	"sync"
 )
 
 var (
@@ -64,10 +66,17 @@ func splitAndStore(reader *bufio.Reader, filePath string) error {
 }
 
 func storeChunkToNode(data []byte, filePath string, chunkIndex int) error {
+	// Calculate checksum
+	hasher := sha256.New()
+	hasher.Write(data)
+	checksum := hasher.Sum(nil)
+
 	serverNode := serverNodes[chunkIndex%len(serverNodes)]
 	chunkFilePath := fmt.Sprintf("%s_%d.chunk", filePath, chunkIndex)
-	log.Printf("Storing chunk %d to %s\n", chunkIndex, serverNode)
-	return os.WriteFile(chunkFilePath, data, 0644)
+	log.Printf("Storing chunk %d (Checksum: %x) to %s\n", chunkIndex, checksum, serverNode)
+	// Store checksum with data
+	chunkDataWithChecksum := append(checksum, data...)
+	return os.WriteFile(chunkFilePath, chunkDataWithChecksum, 0644)
 }
 
 func retrieveFile(baseFilePath, destinationFilePath string) error {
@@ -82,21 +91,55 @@ func retrieveFile(baseFilePath, destinationFilePath string) error {
 
 func reassembleFile(destFile *os.File, baseFilePath string) error {
 	chunkIndex := 0
-	for {
-		chunkFilePath := fmt.Sprintf("%s_%d.chunk", baseFilePath, chunkIndex)
-		chunk, err := os.ReadFile(chunkFilePath)
-		if err == os.ErrNotExist {
-			break
-		} else if err != nil {
-			return err
-		}
+	var wg sync.WaitGroup
+	chunkChan := make(chan []byte)
 
-		if _, writeErr := destFile.Write(chunk); writeErr != nil {
-			return writeErr
+	// Process chunks in parallel
+	go func() {
+		for chunk := range chunkChan {
+			if _, writeErr := destFile.Write(chunk); writeErr != nil {
+				log.Printf("Error writing chunk to file: %v", writeErr)
+				continue
+			}
 		}
+	}()
+
+	for {
+		wg.Add(1)
+		chunkFilePath := fmt.Sprintf("%s_%d.chunk", baseFilePath, chunkIndex)
+		go func(chunkFilePath string, index int) {
+			defer wg.Done()
+			chunkDataWithChecksum, err := os.ReadFile(chunkFilePath)
+			if err == os.ErrNotExist {
+				close(chunkChan)
+				return
+			} else if err != nil {
+				log.Printf("Error reading chunk: %v", err)
+				return
+			}
+
+			if len(chunkDataWithChecksum) <= sha256.Size {
+				log.Printf("Invalid chunk size, skipping: %s", chunkFilePath)
+				return
+			}
+
+			checksum, data := chunkDataWithChecksum[:sha256.Size], chunkDataWithChecksum[sha256.Size:]
+			calculatedChecksum := sha256.Sum256(data)
+			if !bytes.Equal(checksum, calculatedChecksum[:]) {
+				log.Printf("Checksum mismatch, skipping chunk: %s", chunkFilePath)
+				return
+			}
+
+			chunkChan <- data
+		}(chunkFilePath, chunkIndex)
+
 		chunkIndex++
+		if chunkIndex%len(serverNodes) == 0 { // Assume each file chunk retrieval starts after the previous one
+			time.Sleep(1 * time.Second) // Simulate network/disk latency
+		}
 	}
 
+	wg.Wait() // Wait for all go routines to finish
 	return nil
 }
 
